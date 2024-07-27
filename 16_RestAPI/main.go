96ff0d42cb26c285ef38d6280231e64f45666ec1
yyogesh <yogeshggn@hotmail.com>
2024-07-27 11:11:52 +0530

    golang rest api crud

--- 16_RestAPI/main.go
+++ 16_RestAPI/main.go
@@ -2,24 +2,50 @@ package main
 
 import (
 	"net/http"
+	"restapi_example/db"
 	"restapi_example/models"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() { //com component, remoting, webservice soap based service, rest service, graphql, gRpc
+	db.InitDB()
 	server := gin.Default()
 
 	server.GET("/", helloWorldHandler)
 	server.GET("/home", homeHandler)
 	server.GET("/events", getEvents)
+	server.GET("/event/:id", getEvent)
 	server.POST("/events", createEvents)
 
 	server.Run(":8080") // listen and serve on 0.0.0.0:8080
 }
 
+func getEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Invalid event ID"})
+		return
+	}
+
+	event, err := models.GetEventById(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusNotFound, gin.H{"error": "Event not found"})
+		return
+	}
+
+	context.JSON(http.StatusOK, event)
+}
+
 func getEvents(context *gin.Context) {
-	events := models.GetEvents()
+	events, err := models.GetAllEvents()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
 	context.JSON(http.StatusOK, events)
 }
 
@@ -35,8 +61,11 @@ func createEvents(context *gin.Context) { // request postman body raw json forma
 	event.ID = 1
 	event.UserId = 1
 
-	event.Save()
-
+	err = event.Save()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event}) // 201 Created status code
 
 }
--- 16_RestAPI/models/event.go
+++ 16_RestAPI/models/event.go
@@ -1,6 +1,11 @@
 package models
 
-import "time"
+import (
+	"database/sql"
+	"fmt"
+	"restapi_example/db"
+	"time"
+)
 
 type Event struct {
 	ID          int64
@@ -13,10 +18,64 @@ type Event struct {
 
 var events = []Event{}
 
-func (e Event) Save() {
-	events = append(events, e) // events = [...events, newEvent]
+func (e Event) Save() error {
+	//events = append(events, e) // events = [...events, newEvent]
+	query := `
+	INSERT INTO events(name, description, location, dateTime, user_id) 
+	VALUES (?, ?, ?, ?, ?)`
+
+	stmt, err := db.DB.Prepare(query)
+
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserId)
+
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId()
+	e.ID = id
+	fmt.Println(id, e.Name, e.Description, e.Location, e.DateTime)
+	return err
 }
 
-func GetEvents() []Event {
-	return events
+func GetAllEvents() ([]Event, error) {
+	query := `SELECT * FROM events`
+	rows, err := db.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var events []Event
+
+	for rows.Next() {
+		var event Event
+		err := rows.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserId)
+		if err != nil {
+			return nil, err
+		}
+		events = append(events, event)
+	}
+
+	return events, nil
+}
+
+func GetEventById(id int64) (*Event, error) {
+	query := `SELECT * FROM events WHERE id = ?`
+	row := db.DB.QueryRow(query, id)
+
+	var event Event
+	err := row.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserId)
+
+	if err == sql.ErrNoRows {
+		return nil, fmt.Errorf("event not found with id %d", id)
+	} else if err != nil {
+		return nil, err
+	}
+
+	return &event, nil
 }
